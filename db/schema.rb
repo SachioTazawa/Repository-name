1a9cb6feff6538e9232da575cdbbcb153869e418
SachioTazawa <sa.tazawa.engi@gmail.com>
2021-06-13 14:02:25 +0000

    Finish full-fledged-login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210609104331) do
+ActiveRecord::Schema.define(version: 20210612143452) do
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'sstring' for column 'password_digest'
